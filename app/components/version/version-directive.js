adb7591d3c2617cb03a97f20fe61627dadad2543
lykovrs <lykovrs@gmail.com>
2016-07-27 14:49:26 +0300

    add webpack and main-page contetnt

--- app/components/version/version-directive.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('labApp.version.version-directive', [])
-
-.directive('appVersion', ['version', function(version) {
-  return function(scope, elm, attrs) {
-    elm.text(version);
-  };
-}]);
--- app/components/version/version.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('labApp.version', [
-  'labApp.version.interpolate-filter',
-  'labApp.version.version-directive'
-])
-
-.value('version', '0.1');
